f9f96aeb0919ab25ff62a39b94acdc5ccfd41781
Nick Parks <91085168+CaptainFarce@users.noreply.github.com>
2022-05-06 13:42:15 -0500

    Update Item.java

--- Solution/src/main/java/com/example/Item.java
+++ Solution/src/main/java/com/example/Item.java
@@ -2,14 +2,62 @@ package com.example;
 
 public class Item{
     
+	private String itemName;
+	private String itemDesc;
+	private double itemPrice;
+	private int quantity;
+	private int availableQuantity;
     
     public Item() {
-        // Your code here
+        quantity = 0;
     }
     
     public Item(String itemName, String itemDesc, Double itemPrice, Integer availableQuantity) {
-        // Your code here
+        this.itemName = itemName;
+        this.itemDesc = itemDesc;
+        this.itemPrice = itemPrice;
+        this.availableQuantity = availableQuantity;
     }
+
+	public String getItemName() {
+		return itemName;
+	}
+
+	public void setItemName(String itemName) {
+		this.itemName = itemName;
+	}
+
+	public String getItemDesc() {
+		return itemDesc;
+	}
+
+	public void setItemDesc(String itemDesc) {
+		this.itemDesc = itemDesc;
+	}
+
+	public double getItemPrice() {
+		return itemPrice;
+	}
+
+	public void setItemPrice(double itemPrice) {
+		this.itemPrice = itemPrice;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	public int getAvailableQuantity() {
+		return availableQuantity;
+	}
+
+	public void setAvailableQuantity(int availableQuantity) {
+		this.availableQuantity = availableQuantity;
+	}
     
     
     
